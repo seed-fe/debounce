d6749bf51a7a6f1ebf3a6c17be7331be4ad9921c
seed-fe <zzlfullstack@gmail.com>
2017-05-08 22:46:46 +0800

    完成了向量函数和向量叉乘函数

---
+++ src/script/function.js
@@ -0,0 +1,13 @@
+// 向量
+function vector (a, b) {
+    // body...
+    return {
+        x: b.x - a.x,
+        y: b.y - a.y
+    }
+}
+// 向量叉乘
+function vectorProduct (arguments) {
+    // body...
+    return v1.x * v2.y - v2.x * v1.y
+}
